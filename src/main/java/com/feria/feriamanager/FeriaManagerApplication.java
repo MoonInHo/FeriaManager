943509baee67e99ae920a4bb29432d74f37fcb8a
MoonInHo <ansdlsgh55@gmail.com>
2023-11-06 22:28:13 +0900

    first commit

---
+++ src/main/java/com/feria/feriamanager/FeriaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.feria.feriamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriaManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeriaManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/feria/feriamanager/FeriaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feria.feriamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriaManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
